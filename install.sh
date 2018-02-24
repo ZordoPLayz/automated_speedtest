289eb74c505db1c0048598547808328be76396c6
ZordoPlayz <25035074+ZordoPlayz@users.noreply.github.com>
2018-02-24 19:11:58 +0100

    Fixed a bit

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 pwd= < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-12};
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password ${pwd}'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ${pwd}'
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password ${pwd}"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${pwd}"
 sudo apt-get install -y python3 python3-pip apache2 libapache2-mod-php7.0 php7.0 php7.0-mysql mysql-server
+pip3 install speedtest-cli
+
+
+
+echo "Password for the MySQL-Server: ${pwd}"
