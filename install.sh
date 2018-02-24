7c8d668a5f09e0794c393d8b6e911f15a2c67257
ZordoPlayz <25035074+ZordoPlayz@users.noreply.github.com>
2018-02-24 18:39:11 +0100

    Added missing dependency

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt-get upgrade -y
 pwd= < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-12};
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password ${pwd}'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ${pwd}'
-sudo apt-get install -y python apache2 libapache2-mod-php7.0 php7.0 php7.0-mysql mysql-server
+sudo apt-get install -y python3 python3-pip apache2 libapache2-mod-php7.0 php7.0 php7.0-mysql mysql-server
